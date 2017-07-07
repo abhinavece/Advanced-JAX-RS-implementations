3578ece73a7ba0e20345eb87e00f5d5a8061eb8e
Abhinav <singh09.abhinav@gmail.com>
2017-07-07 10:26:36 +0530

    changed files checked in.

---
+++ src/main/java/com/heapdev/restapi/MyApp.java
@@ -0,0 +1,9 @@
+package com.heapdev.restapi;
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+
+@ApplicationPath("restapi")
+public class MyApp extends Application {
+	
+}
--- src/main/java/com/heapdev/restapi/MyResource.java
+++ src/main/java/com/heapdev/restapi/MyResource.java
@@ -1,17 +1,22 @@
 package com.heapdev.restapi;
 
+import javax.inject.Singleton;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 
-@Path("/test")
+@Path("test")
+@Singleton
 public class MyResource {
+	
+	private int count;
 	
 	@GET
 	@Produces(MediaType.TEXT_PLAIN)
 	public String test(){
-		return "test succeeded";
+		count = count+1;
+		return "Total number of request is: "+ count;
 	}
 
 }
