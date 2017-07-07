2e477b87b3647042b30846bc46c79a95159c61ee
Abhinav <singh09.abhinav@gmail.com>
2017-07-07 14:01:04 +0530

    added functionality

--- src/main/java/com/heapdev/restapi/MyResource.java
+++ src/main/java/com/heapdev/restapi/MyResource.java
@@ -3,20 +3,29 @@ package com.heapdev.restapi;
 import javax.inject.Singleton;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
+import javax.ws.rs.QueryParam;
 import javax.ws.rs.core.MediaType;
 
-@Path("test")
-@Singleton
+@Path("{pathParam}/test")
 public class MyResource {
 	
-	private int count;
+	/**
+	 * For Singleton class, instance is created before the request is made. So the queryParam and pathParam will throw the error.
+	 */
+	
+	/**
+	 * For 
+	 */
+	
+	@PathParam("pathParam") private String pathParam;
+	@QueryParam("queryParam") private String queryParam;
 	
 	@GET
 	@Produces(MediaType.TEXT_PLAIN)
 	public String test(){
-		count = count+1;
-		return "Total number of request is: "+ count;
+		return " PathParam: "+pathParam+ ", queryParam: "+queryParam;
 	}
 
 }
